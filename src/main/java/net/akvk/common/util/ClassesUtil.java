ec8edfefd62c40525c9fe329d15641ae8310b51b
abhishek <Abhishek.VK@ge.com>
2019-01-18 09:55:17 +0530

    ClassesUtil - Create list of objects with default constructor CommandUtil - Run process in background with ProcessBuilder.

--- src/main/java/net/akvk/common/util/ClassesUtil.java
+++ src/main/java/net/akvk/common/util/ClassesUtil.java
@@ -13,94 +13,103 @@ import org.slf4j.LoggerFactory;
 
 public class ClassesUtil implements FileWatcher {
 
-  private static final String CLASSES_FILE = "classes.properties";
-  private static Properties classes = null;
+    private static final String CLASSES_FILE = "classes.properties";
+    private static Properties classes = null;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ClassesUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassesUtil.class);
 
-  private static final ReadWriteLock RWL = new ReentrantReadWriteLock();
-  private static final Lock RLOCK = RWL.readLock();
-  private static final Lock WLOCK = RWL.writeLock();
+    private static final ReadWriteLock RWL = new ReentrantReadWriteLock();
+    private static final Lock RLOCK = RWL.readLock();
+    private static final Lock WLOCK = RWL.writeLock();
 
-  static {
-    loadClasses();
-  }
-
-  private static void loadClasses() {
-    try (InputStream is = FileUtil.getConfigStream(CLASSES_FILE)) {
-      classes = new Properties();
-      classes.load(is);
-    } catch (IOException e) {
-      classes = null;
-      LOGGER.error("loadClasses - " + e.toString());
+    static {
+        loadClasses();
     }
-  }
 
-  public static String getClassName(String propertyName) {
-    String retVal = null;
-    RLOCK.lock();
-    try {
-      if (classes.containsKey(propertyName)) {
-        retVal = classes.getProperty(propertyName);
-      }
-    } finally {
-      RLOCK.unlock();
+    private static void loadClasses() {
+        try (InputStream is = FileUtil.getConfigStream(CLASSES_FILE)) {
+            classes = new Properties();
+            classes.load(is);
+        } catch (IOException e) {
+            classes = null;
+            LOGGER.error("loadClasses - " + e.toString());
+        }
     }
-    return retVal;
-  }
 
-  public static String[] getClassesName(String propertyName) {
-    String[] retVal = null;
-    String value = getClassName(propertyName);
-    if (value != null) {
-      retVal = value.split(",");
-    } else {
-			retVal = new String[0];
-		}
-    return retVal;
-  }
-
-  public static Class getClassFromName(String propertyName) throws Exception {
-    String value = getClassName(propertyName);
-    return Class.forName(value);
-  }
-
-  public static List<Class> getClassesFromName(String propertyName) throws Exception {
-    List<Class> retVal = new ArrayList();
-    String value = getClassName(propertyName);
-    if (value != null) {
-      String[] clazz = value.split(",");
-      for (String cls : clazz) {
-        retVal.add(Class.forName(cls.trim()));
-      }
+    public static String getClassName(String propertyName) {
+        String retVal = null;
+        RLOCK.lock();
+        try {
+            if (classes.containsKey(propertyName)) {
+                retVal = classes.getProperty(propertyName);
+            }
+        } finally {
+            RLOCK.unlock();
+        }
+        return retVal;
     }
-    return retVal;
-  }
-	
-	public static Object getObjectFromName(String propertyName) throws Exception{
-		Class clazz = getClassFromName(propertyName);
-		return clazz.getConstructor().newInstance();
-	}
-	
-	public static Object getObjectFromName(String propertyName, Class argType, Object value) throws Exception{
-		Class clazz = getClassFromName(propertyName);
-		return clazz.getConstructor(argType).newInstance(value);
-	}
 
-  @Override
-  public void onFileModified() {
-    LOGGER.info("onFileModified - " + CLASSES_FILE);
-    WLOCK.lock();
-    try {
-      classes.clear();
-      loadClasses();
-    } finally {
-      WLOCK.unlock();
+    public static String[] getClassesName(String propertyName) {
+        String[] retVal = null;
+        String value = getClassName(propertyName);
+        if (value != null) {
+            retVal = value.split(",");
+        } else {
+            retVal = new String[0];
+        }
+        return retVal;
     }
-  }
 
-  @Override
-  public String getWatchedFilename() {
-    return CLASSES_FILE;
-  }
+    public static Class getClassFromName(String propertyName) throws Exception {
+        String value = getClassName(propertyName);
+        return Class.forName(value);
+    }
+
+    public static List<Class> getClassesFromName(String propertyName) throws Exception {
+        List<Class> retVal = new ArrayList();
+        String value = getClassName(propertyName);
+        if (value != null) {
+            String[] clazz = value.split(",");
+            for (String cls : clazz) {
+                retVal.add(Class.forName(cls.trim()));
+            }
+        }
+        return retVal;
+    }
+
+    public static Object getObjectFromName(String propertyName) throws Exception {
+        Class clazz = getClassFromName(propertyName);
+        return clazz.getConstructor().newInstance();
+    }
+
+    public static Object getObjectFromName(String propertyName, Class argType, Object value) throws Exception {
+        Class clazz = getClassFromName(propertyName);
+        return clazz.getConstructor(argType).newInstance(value);
+    }
+
+    public static List<Object> getObjectsFromName(String propertyName) throws Exception {
+        List<Class> clazzez = getClassesFromName(propertyName);
+        List<Object> objects = new ArrayList(clazzez.size());
+        for(Class clazz : clazzez) {
+            objects.add(clazz.getConstructor().newInstance());
+        }
+        return objects;
+    }
+
+    @Override
+    public void onFileModified() {
+        LOGGER.info("onFileModified - " + CLASSES_FILE);
+        WLOCK.lock();
+        try {
+            classes.clear();
+            loadClasses();
+        } finally {
+            WLOCK.unlock();
+        }
+    }
+
+    @Override
+    public String getWatchedFilename() {
+        return CLASSES_FILE;
+    }
 }
--- src/main/java/net/akvk/common/util/CommandUtil.java
+++ src/main/java/net/akvk/common/util/CommandUtil.java
@@ -1,8 +1,10 @@
 package net.akvk.common.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -14,95 +16,166 @@ import org.slf4j.LoggerFactory;
 
 public class CommandUtil implements FileWatcher {
 
-  private static final String CMD_FILE = "commands.properties";
-  private static Properties commands;
+    private static final String CMD_FILE = "commands.properties";
+    private static Properties commands;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(CommandUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandUtil.class);
 
-  private static final ReadWriteLock RWL = new ReentrantReadWriteLock();
-  private static final Lock RLOCK = RWL.readLock();
-  private static final Lock WLOCK = RWL.writeLock();
+    private static final ReadWriteLock RWL = new ReentrantReadWriteLock();
+    private static final Lock RLOCK = RWL.readLock();
+    private static final Lock WLOCK = RWL.writeLock();
 
-  static {
-    loadCommands();
-  }
-
-  private static void loadCommands() {
-    try (InputStream is = FileUtil.getConfigStream(CMD_FILE)) {
-      commands = new Properties();
-      commands.load(is);
-    } catch (Exception e) {
-      commands = null;
-      LOGGER.error("loadCommands - " + e.toString());
+    static {
+        loadCommands();
     }
-  }
 
-  private static String getCommand(String key) {
-    RLOCK.lock();
-    try {
-      return commands.getProperty(key);
-    } finally {
-      RLOCK.unlock();
-    }
-  }
-
-  public static String executeCommand(String cmd) throws Exception {
-    StringBuilder retVal = new StringBuilder("");
-    String line;
-    if (cmd != null && cmd.trim().length() > 0) {
-      String newLine = System.lineSeparator();
-      Process process = Runtime.getRuntime().exec(cmd);
-      try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-        while ((line = in.readLine()) != null) {
-          retVal.append(line.trim()).append(newLine);
+    private static void loadCommands() {
+        try (InputStream is = FileUtil.getConfigStream(CMD_FILE)) {
+            commands = new Properties();
+            commands.load(is);
+        } catch (Exception e) {
+            commands = null;
+            LOGGER.error("loadCommands - " + e.toString());
         }
-        LOGGER.info("Exit value : " + process.waitFor());
-        process.destroy();
-      } catch (Exception e) {
-        LOGGER.error("executeCommand - " + e);
-        throw e;
-      }
     }
-    return retVal.toString();
-  }
 
-  public static String execute(String cmdKey) throws Exception {
-    return executeCommand(getCommand(cmdKey));
-  }
-
-  public static String execute(String cmdKey, String target, String replacement) throws Exception {
-    String command = getCommand(cmdKey);
-    if (command != null && target != null) {
-      command = command.replaceAll(target, replacement);
+    private static String getCommand(String key) {
+        if (key == null || key.length() == 0) {
+            throw new NullPointerException("Key is null or empty. Please provide a valid command key.");
+        }
+        RLOCK.lock();
+        try {
+            return commands.getProperty(key);
+        } finally {
+            RLOCK.unlock();
+        }
     }
-    return execute(command);
-  }
 
-  public static String execute(String cmdKey, Map<String, String> substitution) throws Exception {
-    String command = getCommand(cmdKey);
-    if (command != null && substitution != null) {
-      Set<String> targets = substitution.keySet();
-      for (String target : targets) {
-        command = command.replaceAll(target, substitution.get(target));
-      }
+    public static String executeCommand(String cmd) throws Exception {
+        StringBuilder retVal = new StringBuilder("");
+        String line;
+        if (cmd != null && cmd.trim().length() > 0) {
+            String newLine = System.lineSeparator();
+            Process process = Runtime.getRuntime().exec(cmd);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                while ((line = in.readLine()) != null) {
+                    retVal.append(line.trim()).append(newLine);
+                }
+                LOGGER.info("Exit value : " + process.waitFor());
+                process.destroy();
+            } catch (Exception e) {
+                LOGGER.error("executeCommand - " + e);
+                throw e;
+            }
+        }
+        return retVal.toString();
     }
-    return executeCommand(command);
-  }
 
-  @Override
-  public void onFileModified() {
-    LOGGER.info("onFileModified - " + CMD_FILE);
-    WLOCK.lock();
-    try {
-      commands.clear();
-      loadCommands();
-    } finally {
-      WLOCK.unlock();
+    public static String execute(String cmdKey) throws Exception {
+        return executeCommand(getCommand(cmdKey));
     }
-  }
 
-  @Override
-  public String getWatchedFilename() {
-    return CMD_FILE;
-  }
+    public static String execute(String cmdKey, String target, String replacement) throws Exception {
+        String command = getCommand(cmdKey);
+        if (command != null && target != null) {
+            command = command.replaceAll(target, replacement);
+        }
+        return execute(command);
+    }
+
+    public static String execute(String cmdKey, Map<String, String> substitution) throws Exception {
+        String command = getCommand(cmdKey);
+        if (command != null && substitution != null) {
+            Set<String> targets = substitution.keySet();
+            for (String target : targets) {
+                command = command.replaceAll(target, substitution.get(target));
+            }
+        }
+        return executeCommand(command);
+    }
+
+    public static String execute(String cmdKey, List<String> arguments) throws Exception {
+        String command = getCommand(cmdKey);
+        if (command != null && arguments != null && arguments.size() > 0) {
+            StringBuilder sb = new StringBuilder(command);
+            sb.append(" ").append(String.join(" ", arguments));
+            command = sb.toString();
+        }
+        return executeCommand(command);
+    }
+
+    public static String execute(String cmdKey, String argument) throws Exception {
+        String command = getCommand(cmdKey);
+        if (command != null && argument != null && argument.length() > 0) {
+            StringBuilder sb = new StringBuilder(command);
+            sb.append(" ").append(argument);
+            command = sb.toString();
+        }
+        return executeCommand(command);
+    }
+
+    public static Process executeProcess(String command) throws IOException {
+        ProcessBuilder pBuilder = new ProcessBuilder(command);
+        return pBuilder.start();
+    }
+
+    public static Process executeBackgroundProcess(String cmdKey) throws IOException {
+        return executeProcess(getCommand(cmdKey));
+    }
+
+    public static Process executeBackgroundProcess(String cmdKey, String target, String replacement) throws IOException {
+        String command = getCommand(cmdKey);
+        if (command != null && target != null) {
+            command = command.replaceAll(target, replacement);
+        }
+        return executeProcess(command);
+    }
+
+    public static Process executeBackgroundProcess(String cmdKey, Map<String, String> substitution) throws IOException {
+        String command = getCommand(cmdKey);
+        if (command != null && substitution != null) {
+            Set<String> targets = substitution.keySet();
+            for (String target : targets) {
+                command = command.replaceAll(target, substitution.get(target));
+            }
+        }
+        return executeProcess(command);
+    }
+
+    public static Process executeBackgroundProcess(String cmdKey, List<String> arguments) throws IOException {
+        String command = getCommand(cmdKey);
+        if (command != null && arguments != null && arguments.size() > 0) {
+            StringBuilder sb = new StringBuilder(command);
+            sb.append(" ").append(String.join(" ", arguments));
+            command = sb.toString();
+        }
+        return executeProcess(command);
+    }
+
+    public static Process executeBackgroundProcess(String cmdKey, String argument) throws IOException {
+        String command = getCommand(cmdKey);
+        if (command != null && argument != null && argument.length() > 0) {
+            StringBuilder sb = new StringBuilder(command);
+            sb.append(" ").append(argument);
+            command = sb.toString();
+        }
+        return executeProcess(command);
+    }
+
+    @Override
+    public void onFileModified() {
+        LOGGER.info("onFileModified - " + CMD_FILE);
+        WLOCK.lock();
+        try {
+            commands.clear();
+            loadCommands();
+        } finally {
+            WLOCK.unlock();
+        }
+    }
+
+    @Override
+    public String getWatchedFilename() {
+        return CMD_FILE;
+    }
 }
